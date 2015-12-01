37dbfd78cdc3657d9b126dfa3940eea150650c15
Jo Colina <jo@jocolina.com>
2015-12-01 17:14:02 +0100

    Performance Graph Test

--- public/js/lib/graphs.js
+++ public/js/lib/graphs.js
@@ -25,6 +25,17 @@ $("#perf-chart").highcharts({
 		threshold:0,
 		negativeColor: "#FF5555",
 		color: "#55FF55",
+		name: '',
 		data: [[0,1], [1,2], [2,-3],[3,-3], [4,0], [5,1],[6,-2],[7,3],[8,9]]
-	}]
+	}],
+	yAxis:{
+		title:{
+			text: "Balance"
+		}
+	},
+	xAxis:{
+		title:{
+			text: "Semaines"
+		}
+	}
 });
